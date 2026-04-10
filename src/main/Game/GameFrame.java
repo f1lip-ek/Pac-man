9dca11b1eb298fa9f30eae9da930796bad38a75f
Filip <filipzitek@seznam.cz>
2026-04-10 19:53:00 +0200

    adding GamePanel and into GameFrame class

--- src/main/Game/GameFrame.java
+++ src/main/Game/GameFrame.java
@@ -3,4 +3,17 @@ package main.Game;
 import javax.swing.*;
 
 public class GameFrame extends JFrame {
+
+    private GamePanel gamePanel;
+
+    public GameFrame(){
+        gamePanel = new GamePanel();
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.add(gamePanel);
+
+
+        this.pack();
+        this.setVisible(true);
+    }
 }
---
+++ src/main/Game/GamePanel.java
@@ -0,0 +1,13 @@
+package main.Game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(800, 600));
+        this.setBackground(Color.BLUE);
+    }
+
+}
