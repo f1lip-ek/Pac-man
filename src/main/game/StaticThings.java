5ef7d289319f6b35be0c18f5b4d7d778366d61ac
Filip <filipzitek@seznam.cz>
2026-05-13 21:48:21 +0200

    moved method from Ghost class to get all possible movements to move

--- src/main/game/StaticThings.java
+++ src/main/game/StaticThings.java
@@ -1,9 +1,15 @@
 package main.game;
 
+import main.game.entities.Movement;
+import main.game.entities.Player;
+import main.game.levels.Level;
+
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class StaticThings {
 
@@ -15,5 +21,34 @@ public class StaticThings {
             System.err.println("Error loading image: " + e.getMessage());
         }
         return img;
+    }
+
+    public static ArrayList<Movement> getMovements(Level level, Rectangle hitbox, float speed, Movement lastMovement){
+        ArrayList<Movement> list = new ArrayList<>();
+
+        if (!level.isWall(hitbox.x, (int) (hitbox.y - speed)) &&
+                !level.isWall(hitbox.x + hitbox.width - 1, (int) (hitbox.y - speed))){
+            list.add(Movement.UP);
+        }
+        if (!level.isWall(hitbox.x, (int) (hitbox.y + hitbox.height - 1 + speed)) &&
+                !level.isWall(hitbox.x + hitbox.width - 1, (int) (hitbox.y + hitbox.height - 1 + speed))) {
+            list.add(Movement.DOWN);
+        }
+        if (!level.isWall((int) (hitbox.x - speed), hitbox.y) &&
+                !level.isWall((int) (hitbox.x - speed), hitbox.y + hitbox.height - 1)){
+            list.add(Movement.LEFT);
+        }
+        if (!level.isWall((int) (hitbox.x + hitbox.width - 1 + speed), hitbox.y) &&
+                !level.isWall((int) (hitbox.x + hitbox.width - 1 + speed), hitbox.y + hitbox.height - 1)){
+            list.add(Movement.RIGHT);
+        }
+
+        switch (lastMovement) {
+            case Movement.UP -> list.remove(Movement.UP);
+            case Movement.DOWN -> list.remove(Movement.DOWN);
+            case Movement.LEFT -> list.remove(Movement.LEFT);
+            case Movement.RIGHT -> list.remove(Movement.RIGHT);
+        }
+        return list;
     }
 }
